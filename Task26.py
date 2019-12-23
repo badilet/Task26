7d51692a58a75262db81bf2c3bbb97b2c9372244
badilet <58660513+badilet@users.noreply.github.com>
2019-12-23 04:16:32 -0600

    Add files via upload

--- Task26.py
+++ Task26.py
@@ -1,11 +1,9 @@
 # Напишите функцию которая подсчитает количество счетных и несчетных чисел в списке
 # чисел.
-num_list = [2, 4, 3, 43, 65, 87, 21, 98, 424, 457, 98585, 1132, 5253545, 89]
-# def num_count():
-even_num = []
-odd_num = []
-if i in num_list and i % 2 == 0:
-    even_num.append(i)
-else:
-    odd_num.append(i)
 
+num_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+even = [x for x in num_list if x % 2 == 0]
+print(len(even))
+
+odd = [x for x in num_list if x % 2 != 0]
+print(len(odd))
